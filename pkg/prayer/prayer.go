d81bfab57917b936d524b9ad24bed03865f78eb5
escalopa <ahmad.helaly.dev@gmail.com>
2023-01-05 06:54:50 +0300

    Add Multilanguage support

--- pkg/prayer/prayer.go
+++ pkg/prayer/prayer.go
@@ -1,35 +1,125 @@
 package prayer
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
 )
 
+type Prayer string
+
 type PrayerTimes struct {
-	Fajr    time.Time `json:"fajr"`
-	Dhuhr   time.Time `json:"dhuhr"`
-	Asr     time.Time `json:"asr"`
-	Maghrib time.Time `json:"maghrib"`
-	Isha    time.Time `json:"isha"`
+	Date    string `json:"date"`
+	Fajr    Prayer `json:"fajr"`
+	Sunrise Prayer `json:"sunrise"`
+	Dhuhr   Prayer `json:"dhuhr"`
+	Asr     Prayer `json:"asr"`
+	Maghrib Prayer `json:"maghrib"`
+	Isha    Prayer `json:"isha"`
 }
 
-func (p *PrayerTimes) EnglishString() string {
+func NewPrayerTimes(date, fajr, sunrise, dhuhr, asr, maghrib, isha string) PrayerTimes {
+	return PrayerTimes{
+		Date:    date,
+		Fajr:    Prayer(fajr),
+		Sunrise: Prayer(sunrise),
+		Dhuhr:   Prayer(dhuhr),
+		Asr:     Prayer(asr),
+		Maghrib: Prayer(maghrib),
+		Isha:    Prayer(isha),
+	}
+}
+
+func (p PrayerTimes) MarshalBinary() ([]byte, error) {
+	return json.Marshal(p)
+}
+
+func (p PrayerTimes) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, &p)
+}
+
+func (p *PrayerTimes) EnString() string {
 	return fmt.Sprintf(
-		"Fajr: %s\nDhuhr: %s\nAsr: %s\nMaghrib: %s\nIsha: %s",
-		p.Fajr, p.Dhuhr, p.Asr, p.Maghrib, p.Isha,
+		`
+		Date    : %s
+		Fajr    : %s
+		Sunrise : %s
+		Dhuhr   : %s
+		Asr     : %s
+		Maghrib : %s
+		Isha    : %s
+		`, p.Date, p.Fajr, p.Sunrise, p.Dhuhr, p.Asr, p.Maghrib, p.Isha,
 	)
 }
 
-func (p *PrayerTimes) RussianString() string {
+// HTML returns prayer times in HTML format
+func (p *PrayerTimes) EnHTML() string {
 	return fmt.Sprintf(
-		"Восход: %s\nЗухр: %s\nАср: %s\nМагриб: %s\nИша: %s",
-		p.Fajr, p.Dhuhr, p.Asr, p.Maghrib, p.Isha,
+		`
+		<b>Date</b>    : %s
+		<b>Fajr</b>    : %s
+		<b>Sunrise</b> : %s
+		<b>Dhuhr</b>   : %s
+		<b>Asr</b>     : %s
+		<b>Maghrib</b> : %s
+		<b>Isha</b>    : %s
+		`, p.Date, p.Fajr, p.Sunrise, p.Dhuhr, p.Asr, p.Maghrib, p.Isha,
 	)
 }
 
-func (p *PrayerTimes) ArabicString() string {
+func (p *PrayerTimes) ArString() string {
 	return fmt.Sprintf(
-		"الفجر: %s\nالظهر: %s\nالعصر: %s\nالمغرب: %s\nالعشاء: %s",
-		p.Fajr, p.Dhuhr, p.Asr, p.Maghrib, p.Isha,
+		`
+		التاريخ : %s
+		الفجر  : %s
+		الشروق : %s
+		الظهر  : %s
+		العصر  : %s
+		المغرب : %s
+		العشاء : %s
+		`, p.Date, p.Fajr, p.Sunrise, p.Dhuhr, p.Asr, p.Maghrib, p.Isha,
+	)
+}
+
+func (p *PrayerTimes) ArHTML() string {
+	return fmt.Sprintf(
+		`
+		<b>التاريخ</b> : %s
+		<b>الفجر</b>  : %s
+		<b>الشروق</b> : %s
+		<b>الظهر </b> : %s
+		<b>العصر</b>  : %s
+		<b>المغرب</b> : %s
+		<b>العشاء</b> : %s
+		
+		`, p.Date, p.Fajr, p.Sunrise, p.Dhuhr, p.Asr, p.Maghrib, p.Isha,
+	)
+}
+
+// Russian
+func (p *PrayerTimes) RuString() string {
+	return fmt.Sprintf(
+		`
+		Дата    : %s
+		Фаджр  : %s
+		Восход : %s
+		Зухр   : %s
+		Аср    : %s
+		Магриб : %s
+		Иша    : %s
+		`, p.Date, p.Fajr, p.Sunrise, p.Dhuhr, p.Asr, p.Maghrib, p.Isha,
+	)
+}
+
+func (p *PrayerTimes) RuHTML() string {
+	return fmt.Sprintf(
+		`
+		%s
+		<b>Фаджр</b>  : %s
+		<b>Восход</b> : %s
+		<b>Зухр </b>  : %s
+		<b>Аср</b>    : %s
+		<b>Магриб</b> : %s
+		<b>Иша</b>    : %s
+		`, p.Date, p.Fajr, p.Sunrise, p.Dhuhr, p.Asr, p.Maghrib, p.Isha,
 	)
 }

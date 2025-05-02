c668aee4a76e9d28e7af390b7a0532f8ed7f3a4a
escalopa <ahmad.helaly.dev@gmail.com>
2025-05-02 21:57:48 +0300

    feat: impl loader fn

---
+++ serverless/loader/internal/parser.go
@@ -0,0 +1,124 @@
+package internal
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/escalopa/prayer-bot/domain"
+)
+
+var (
+	dayFormat   = "2/1/2006"
+	clockFormat = "15:04"
+)
+
+const (
+	prayersCount = 6
+	columnsCount = 7 // (date, fajr, shuruq, dhuhr, asr, maghrib, isha)
+)
+
+func ParsePrayers(file io.Reader, loc *time.Location) (schedule []*domain.PrayerTimes, err error) {
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = columnsCount
+	reader.TrimLeadingSpace = true
+
+	// skip header
+	_, err = reader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	// parse data
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return nil, err
+		}
+
+		prayer, err := parseRecord(record, loc)
+		if err != nil {
+			return nil, err
+		}
+
+		schedule = append(schedule, prayer)
+	}
+
+	return
+}
+
+// parseRecord parses a record from the file
+func parseRecord(record []string, loc *time.Location) (*domain.PrayerTimes, error) {
+	if len(record) != columnsCount {
+		return nil, fmt.Errorf("parseRecord: invalid number of fields, expected 7, got %d", len(record))
+	}
+
+	// parse day date
+	day, err := parseDate(record[0], loc)
+	if err != nil {
+		return nil, err
+	}
+
+	// parse prayers times and convert into time.Time
+	prayers, err := parsePrayer(record[1:], day, loc) // skip first record since it was date
+	if err != nil {
+		return nil, err
+	}
+
+	// add 20 min since `shuruq` is 20 min after sunrise
+	prayers[1] = prayers[1].Add(20 * time.Minute)
+
+	prayer := domain.NewPrayerTimes(
+		day,
+		prayers[0], // fajr
+		prayers[1], // shuruq
+		prayers[2], // dhuhr
+		prayers[3], // asr
+		prayers[4], // maghrib
+		prayers[5], // isha
+	)
+
+	return prayer, nil
+}
+
+// parseDate get day date from string
+func parseDate(line string, loc *time.Location) (time.Time, error) {
+	t, err := time.Parse(dayFormat, line)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("parseDate[%s]: %v", line, err)
+	}
+	return domain.Date(t.Day(), t.Month(), t.Year(), loc), nil
+}
+
+// parsePrayer parses all day's prayers
+func parsePrayer(prayersStr []string, day time.Time, loc *time.Location) ([]time.Time, error) {
+	if len(prayersStr) != prayersCount {
+		return nil, fmt.Errorf("parsePrayer: unexpected number of prayers, expected 6, got %d", len(prayersStr))
+	}
+
+	// convert prayers array to []time.Time
+	prayers := make([]time.Time, prayersCount)
+	for i, prayerTimeStr := range prayersStr {
+		prayer, err := convertToTime(prayerTimeStr, day, loc)
+		if err != nil {
+			return nil, err
+		}
+		prayers[i] = prayer
+	}
+
+	return prayers, nil
+}
+
+// convertToTime converts a string from format `hh:mm` to time.Time.
+func convertToTime(str string, day time.Time, loc *time.Location) (time.Time, error) {
+	clock, err := time.Parse(clockFormat, str)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("convertToTime[%s]: %v", str, err)
+	}
+	return domain.DateTime(day, clock, loc), nil
+}
---
+++ serverless/loader/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/escalopa/prayer-bot/domain"
+	"github.com/escalopa/prayer-bot/loader/internal"
+	"github.com/escalopa/prayer-bot/service"
+)
+
+type (
+	Storage interface {
+		Get(ctx context.Context, bucket string, key string) ([]byte, error)
+		LoadBotConfig(ctx context.Context) (map[uint8]*service.BotConfig, error)
+	}
+
+	DB interface {
+		StorePrayers(ctx context.Context, botID uint8, rows []*domain.PrayerTimes) error
+	}
+
+	Event struct {
+		Messages []struct {
+			EventMetadata struct {
+				EventId        string    `json:"event_id"`
+				EventType      string    `json:"event_type"`
+				CreatedAt      time.Time `json:"created_at"`
+				TracingContext struct {
+					TraceId      string `json:"trace_id"`
+					SpanId       string `json:"span_id"`
+					ParentSpanId string `json:"parent_span_id"`
+				} `json:"tracing_context"`
+				CloudId  string `json:"cloud_id"`
+				FolderId string `json:"folder_id"`
+			} `json:"event_metadata"`
+			Details struct {
+				BucketId string `json:"bucket_id"`
+				ObjectId string `json:"object_id"`
+			} `json:"details"`
+		} `json:"messages"`
+	}
+)
+
+const (
+	filenameSuffix = ".csv"
+)
+
+func Handler(ctx context.Context, event *Event) error {
+	fmt.Printf("event received: %v\n", event)
+
+	var (
+		storage Storage
+		db      DB
+		err     error
+	)
+
+	storage, err = service.NewStorage()
+	if err != nil {
+		return fmt.Errorf("create storage client: %w", err)
+	}
+
+	db, err = service.NewDB(ctx)
+	if err != nil {
+		return fmt.Errorf("create db connection: %w", err)
+	}
+
+	botConfig, err := storage.LoadBotConfig(ctx)
+	if err != nil {
+		return fmt.Errorf("load bot config: %w", err)
+	}
+
+	for _, msg := range event.Messages {
+		bucket := msg.Details.BucketId
+		key := msg.Details.ObjectId
+
+		// ignore non csv files
+		if !strings.HasSuffix(key, filenameSuffix) {
+			fmt.Printf("ignore file: %s\n", key)
+			continue
+		}
+
+		fmt.Printf("processing file: %s\n", key)
+
+		botID, err := extractBotID(key)
+		if err != nil {
+			return fmt.Errorf("extract info from filename: %s => %w", key, err)
+		}
+
+		config, ok := botConfig[botID]
+		if !ok {
+			return fmt.Errorf("bot config not found for bot_id: %d", botID)
+		}
+
+		loc, err := time.LoadLocation(config.Location)
+		if err != nil {
+			return fmt.Errorf("load location: %s => %w", config.Location, err)
+		}
+
+		data, err := storage.Get(ctx, bucket, key)
+		if err != nil {
+			return fmt.Errorf("get file from S3: %s => %w", key, err)
+		}
+
+		rows, err := internal.ParsePrayers(bytes.NewReader(data), loc)
+		if err != nil {
+			return fmt.Errorf("load schedule: %s => %w", key, err)
+		}
+
+		err = db.StorePrayers(ctx, botID, rows)
+		if err != nil {
+			return fmt.Errorf("store prayers: %s => %w", key, err)
+		}
+
+		fmt.Printf("processed file for bot_id: %d, location: %s\n", botID, loc)
+	}
+
+	return nil
+}
+
+const (
+	filenameParts = 2
+)
+
+// extractBotID extracts the bot ID from the filename.
+// example filename: "BOT_ID-CITY_NAME.csv"
+func extractBotID(filename string) (uint8, error) {
+	parts := strings.Split(path.Base(filename), "-")
+	if len(parts) != filenameParts {
+		return 0, fmt.Errorf("unexpected filename format: %s", filename)
+	}
+
+	botID, err := strconv.ParseUint(parts[0], 10, 8)
+	if err != nil {
+		return 0, fmt.Errorf("parse bot_id: %s => %w", parts[0], err)
+	}
+
+	return uint8(botID), nil
+}

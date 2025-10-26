65a81fcbc549375e2e47aac1bafc70fc3f350c18
escalopa <ahmed.helaly.dev@gmail.com>
2025-10-26 15:51:54 +0300

    feat: update chat

--- domain/chat.go
+++ domain/chat.go
@@ -5,6 +5,7 @@ import (
 )
 
 var (
+	ErrUnmarshalJSON   = errors.New("unmarshal json")
 	ErrInvalidArgument = errors.New("invalid argument")
 	ErrNotFound        = errors.New("not found")
 	ErrAlreadyExists   = errors.New("already exists")
